06a8d66faf045a1f2d60071d6fbe041ef901a8a7
lambo064 <tasha_lambo@mail.ru>
2023-11-19 12:49:38 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        int number;
+        do {
+            System.out.print("Input a number: ");
+            number = scanner.nextInt();
+            if (number != 1) {
+                System.out.println("didnt guess");
+            }
+        }
+        while (number == 1);
+        scanner.close();
+    }
+
+}
